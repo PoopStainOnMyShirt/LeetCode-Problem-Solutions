331cbb16dc192b11eef42fad9053b7cab8bbe3c1
Joyojhyoty Gupta <joyojhyoty@gmail.com>
2025-12-03 00:33:45 +0530

    Time: 3 ms (71.66%) | Memory: 50.6 MB (59.54%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cs
+++ 13-roman-to-integer/roman-to-integer.cs
@@ -1,14 +1,16 @@
 public class Solution {
     public int RomanToInt(string s) {
         int res = 0;
-        Dictionary<char, int> numMap = new Dictionary<char, int>();
-        numMap['I'] = 1;
-        numMap['V'] = 5;
-        numMap['X'] = 10;
-        numMap['L'] = 50;
-        numMap['C'] = 100;
-        numMap['D'] = 500;
-        numMap['M'] = 1000;
+        Dictionary<char, int> numMap = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
 
         for(int i = 0; i < s.Length; i++)
         {
