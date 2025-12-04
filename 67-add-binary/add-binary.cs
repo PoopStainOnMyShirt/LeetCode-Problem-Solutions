3bc79b17e70d108d4fcd016c4657b56119170e74
Joyojhyoty Gupta <joyojhyoty@gmail.com>
2025-12-04 20:44:39 +0530

    Time: 3 ms (28.94%) | Memory: 41.6 MB (81.53%) - LeetSync

---
+++ 67-add-binary/add-binary.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public string AddBinary(string a, string b) {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        
+        System.Text.StringBuilder result = new System.Text.StringBuilder(); 
+        
+        int carry = 0;
+        
+        while (i >= 0 || j >= 0 || carry == 1)
+        {
+            int x = (i >= 0) ? a[i] - '0' : 0;
+            int y = (j >= 0) ? b[j] - '0' : 0;
+            
+            int total_sum = x + y + carry;
+
+            result.Insert(0, total_sum % 2); 
+            
+            carry = total_sum / 2;
+            
+            i--; 
+            j--;
+        }
+
+        return result.ToString();
+    }
+}
