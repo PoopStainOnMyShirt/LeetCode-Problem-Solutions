afd168da25a769e606f9b2d7b69fa00af19667b4
Joyojhyoty Gupta <joyojhyoty@gmail.com>
2025-12-06 00:08:41 +0530

    Time: 1 ms (100.00%) | Memory: 63.5 MB (76.30%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int currentMax = nums[0];
+        int globalMax = nums[0];
+
+        for(int i = 1; i<nums.Length ;i++)
+        {
+            currentMax = Math.Max(nums[i], currentMax + nums[i]);
+            globalMax = Math.Max(globalMax, currentMax);
+        }
+
+        return globalMax;
+    }
+}
